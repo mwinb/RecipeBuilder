80ceb1958e36947fa2be7dacad2fdee16398d0d0
mwinb <mwinb>
2019-04-22 11:41:15 -0700

    USED rails generate pg 46

--- spec/system/add_recipe_spec.rb
+++ spec/system/add_recipe_spec.rb
@@ -5,6 +5,13 @@ RSpec.describe "adding a recipe", type: :system do
     visit new_recipe_path
     fill_in "Name", with: "Test Pancakes"
     fill_in "Ingredient", with: "Test pancake"
+    fill_in "Amount", with: "1 Test"
+    click_on("Add Ingredient")
+    fill_in "Description", with: "Test the pancake"
+    click_on("Add Step")
+    click_on("Create Recipe")
+    visit recipes_path
+    expect(page).to have_content("Test Pancakes")
   end
 end
 
